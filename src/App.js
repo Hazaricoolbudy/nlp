c36b1f9c01948172225188411f9df29aee4dc4ff
Hazari <79424896+Hazaricoolbudy@users.noreply.github.com>
2023-06-09 08:25:12 +0530

    nlp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <h1>Grammer checker</h1>
+        <div className="form-floating">
+          <textarea
+            class="form-control"
+            placeholder="Leave a comment here"
+            id="floatingTextarea"
+          ></textarea>
+        </div>
+        <button>Convert Past Tense</button>
+        <button>Spell Checker</button>
+      </div>
     </div>
   );
 }
